777ee31ca8f8dc0a85b4c8cb5f156fc6d2bdf565
xaetor <38496427+xaetor@users.noreply.github.com>
2020-05-18 18:10:12 -0300

    Create TP5.py

---
+++ GRUPOO-TM/TP5Python/TP5.py
@@ -0,0 +1,39 @@
+lista=[]
+nueva_lista = []
+nueva_lista2 = []
+num_menor = 0
+num_mayor = 0
+ingreso_usuario=int(input("Ingrese un numero de 4 digitos:"))
+lista.apped(ingreso_usuario)
+
+#Array num menor
+for i in str(ingreso_usuario):
+    nueva_lista.append(i)
+
+#Array Num mayor
+for x in str(ingreso_usuario):
+    nueva_lista2.append(x)
+
+#Numero menor
+for recorrido in range(1,len(nueva_lista)):
+   for posicion in range(len(nueva_lista)- recorrido):
+       if nueva_lista[posicion]> nueva_lista[posicion  + 1]:
+           temp = nueva_lista[posicion]
+           nueva_lista[posicion]=nueva_lista[posicion + 1]
+           nueva_lista[posicion + 1] = temp
+
+#Numero mayor
+for recorrido in range(1,len(nueva_lista)):
+   for posicion in range(len(nueva_lista)- recorrido):
+       if nueva_lista2[posicion]> nueva_lista2[posicion  + 1]:
+           temp = nueva_lista2[posicion]
+           nueva_lista2[posicion]= nueva_lista2[posicion + 1]
+           nueva_lista2[posicion + 1]=temp
+
+num_menor = "".join(nueva_lista)
+num_mayor = "".join(nueva_lista2)
+
+print("")
+print("Numero menor posible: "+ num_menor)
+print("")
+print("Numero mayor posible: "+ num_mayor)
