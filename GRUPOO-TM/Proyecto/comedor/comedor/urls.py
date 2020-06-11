ab9e533bee3d60b08b2db83f0e9ac8a7f13d128b
juancito <juanitolaguna1993@gmail.com>
2020-06-11 09:49:48 -0300

    Primer commit del proyecto

---
+++ GRUPOO-TM/Proyecto/comedor/comedor/urls.py
@@ -0,0 +1,10 @@
+"""comedor URL Configuration
+Archivo de urls general. Administra todos los urls de todas las aplicaciones.
+"""
+from django.contrib import admin
+from django.urls import path, re_path, include
+
+urlpatterns = [
+    re_path(r'^', include('aplicaciones.home.urls')),
+    re_path('admin/', admin.site.urls),
+]
