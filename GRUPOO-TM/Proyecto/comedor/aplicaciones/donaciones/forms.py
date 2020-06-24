87b5e7f55593189d08ef11144152099263d10794
juancitortiz <juanitolaguna1993@gmail.com>
2020-06-24 12:10:37 -0300

    Actualizado módulo donaciones, con sus respectivos templates y actualización de settings.py

---
+++ GRUPOO-TM/Proyecto/comedor/aplicaciones/donaciones/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.forms import ModelForm
+from aplicaciones.donaciones.models import Donacion
+
+class FormaDonacion(forms.Form):
+	donante = forms.CharField(max_length=60,widget=forms.TextInput
+        (attrs={
+            "class": "form-control",
+            "placeholder": "Nombre interesado/a"}
+        ))
+        
+	contacto = forms.CharField(max_length=60,widget=forms.TextInput(
+		attrs={
+			"class": "form-control",
+			"placeholder": "Numero de contacto o mail interesado/a"
+		})
+	)
--- GRUPOO-TM/Proyecto/comedor/aplicaciones/donaciones/urls.py
+++ GRUPOO-TM/Proyecto/comedor/aplicaciones/donaciones/urls.py
@@ -1,21 +1,10 @@
-"""comedor URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from . import views
+
+app_name = 'donaciones_app'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('donaciones/', views.donaciones_index, name="donaciones_index"),
+    path('donaciones/<nombre>/', views.donaciones_seleccion, name="donaciones_seleccion"),
+    path('donaciones/<nombre>/<nombre_producto>/', views.donaciones_donacion, name="donaciones_donacion"),
 ]
