44272c702cbbaffe6a72665b01b5924128210ceb
Juan Ignacio Ortiz Innocenti <60118480+juancitortiz@users.noreply.github.com>
2020-06-30 22:06:36 -0300

    Update models.py

--- GRUPOO-TM/Proyecto/comedor/aplicaciones/comedores/models.py
+++ GRUPOO-TM/Proyecto/comedor/aplicaciones/comedores/models.py
@@ -3,12 +3,10 @@ from django.db import models
 class Producto(models.Model):
     nombre = models.CharField(max_length=50)
     # por ej: litros, kilos, unidades...
-    tipo_cantidad = models.CharField(max_length=40)     
+    tipo_cantidad = models.CharField(max_length=40)
+    # cantidad requerida del mismo
     cantidad = models.IntegerField()
-    # Aun no estoy seguro porque no logro conectar esta parte. Me salen errores en la migracion.
-    # Pendiente...
-    #fecha_creacion = models.DateTimeField(auto_now_add=True, null=True)
-    #ultima_modificacion = models.DateTimeField(auto_now=True)
+    # esta función me ayuda para visuarlizar en el administrador
     def __str__(self):
         return 'Producto ' + self.nombre
     
@@ -17,9 +15,11 @@ class Comedor(models.Model):
     nombre = models.CharField(max_length=30)
     ubicacion = models.CharField(max_length=50)
     descripcion = models.TextField(null=True)    
+    # productos de la clase Producto que se requieren para este comedor
     productos_faltantes = models.ManyToManyField(Producto, related_name="producto", symmetrical=False)
     
     def obtener_demanda(self):
         return self.productos_faltantes.all()
+    # esta función me ayuda para visuarlizar en el administrador
     def __str__(self):
         return 'Comedor ' + self.nombre
